7b82f30219a54f92075bbe4f7bb9c8d24a350317
Paulo Huerta <paulothiagoth@gmail.com>
2022-07-19 16:22:49 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     $dbHost = 'Localhost';
     $dbUsername = 'root'
     $dbPassword = '';
-    $dbName = 'formulario-cadastro';
+    $dbName = 'extremeinfoservice';
 
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
 
